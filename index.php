5bf420b4d00b1eb0b962edec35ca0df1ed48269b
Peter Huszar <huszar.peter@sonrisa.hu>
2017-05-15 21:21:46 +0200

    sample query against local oracle

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 
-echo ("Index");
+$tns = "  
+(DESCRIPTION =
+    (ADDRESS_LIST =
+      (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
+    )
+    (CONNECT_DATA =
+      (SERVICE_NAME = XE)
+    )
+  )
+       ";
+$db_username = "SOCIAL";
+$db_password = "xxx";
 
+echo("<pre>");
+
+try{
+    $conn = new PDO("oci:dbname=".$tns,$db_username,$db_password);
+    foreach($conn->query('SELECT * from FELHASZNALO') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+
+}catch(PDOException $e){
+    echo ($e->getMessage());
+}
+
+echo("</pre>");
 ?>
