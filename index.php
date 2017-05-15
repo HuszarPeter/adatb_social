b7f181df713d4cf9234fcfb181727a38f16f72c1
Peter Huszar <huszar.peter@sonrisa.hu>
2017-05-15 21:41:05 +0200

    edit index.php

--- index.php
+++ index.php
@@ -1,23 +1,13 @@
 <?php
+echo("<pre>");
 
-$tns = "  
-(DESCRIPTION =
-    (ADDRESS_LIST =
-      (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
-    )
-    (CONNECT_DATA =
-      (SERVICE_NAME = XE)
-    )
-  )
-       ";
+$tns = "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)) ) (CONNECT_DATA = (SERVICE_NAME = XE) ) )";
 $db_username = "SOCIAL";
 $db_password = "xxx";
 
-echo("<pre>");
-
 try{
-    $conn = new PDO("oci:dbname=".$tns,$db_username,$db_password);
-    foreach($conn->query('SELECT * from FELHASZNALO') as $row) {
+    $conn = new PDO("oci:dbname=".$tns.";charset=utf8",$db_username,$db_password);
+    foreach($conn->query('SELECT FELHASZNALO_NEV from FELHASZNALO') as $row) {
         print_r($row);
     }
     $dbh = null;
