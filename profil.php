8d76e11f58bb9e98b0f6b9d38f85e551aea79c39
ikokoh <ikokoh@users.noreply.github.com>
2017-05-18 23:31:10 +0200

    profil edit meg nem jo

--- profil.php
+++ profil.php
@@ -22,20 +22,23 @@ require_once("lib/functions.php");
     ?>
     <h1>Profil</h1>
     <form method="post" action="profil_edit.php" class="profil">
-        <div class="lbl">Felhasználónév</div>
-        <div><?php echo($user["FELHASZNALO_NEV"]);?></div>
+        <div class="lbl">Felhasználónév:</div>
+        <input type="text" name="usernev" value="<?php echo($user["FELHASZNALO_NEV"]);?>" required ><br/>
+     
+        <div class="lbl">Email:</div>
+        <input type="email" name="email" value="<?php echo($user["EMAIL"]);?>" required ><br/>
 
-        <div class="lbl">Email</div>
-        <div><?php echo($user["EMAIL"]);?></div>
+        <div class="lbl">Név:</div>
+        <input type="text" name="vezeteknev" value="<?php echo($user["VEZETEKNEV"]);?>" required ><br/>
+        <input type="text" name="keresztnev" value="<?php echo($user["KERESZTNEV"]);?>" required ><br/>
 
-        <div class="lbl">Név</div>
-        <div><?php echo($user["VEZETEKNEV"]);?> <?php echo($user["KERESZTNEV"]);?></div>
-
-        <div class="lbl">Születésnap</div>
+        <div class="lbl">Születésnap:</div>
         <div><?php echo($user["SZULETETT"]);?></div>
+        <input type="date" name="datum" value="2013-01-22" required ><br/>
 
-        <div class="lbl">Névnap</div>
+        <div class="lbl">Névnap:</div>
         <div><?php echo($honapok[$user["NEVNAPHONAP"]-1]);?> <?php echo($user["NEVNAPNAP"]);?></div>
+        <input type="number" name="nevnaphu" value="<?php echo($user["NEVNAPHONAP"]);?>" required ><br/>
 
         <hr/>
         <br/><button type="submit">OK</button>
