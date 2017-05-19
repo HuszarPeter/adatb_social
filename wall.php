fe4bcf7cf22ae45ed9cd93fd55276ea528d704a6
Peter Huszar <huszar.peter@sonrisa.hu>
2017-05-19 22:05:15 +0200

    show posted images

--- wall.php
+++ wall.php
@@ -17,17 +17,34 @@ require_once("lib/functions.php");
         <button type="submit">Mehet</button>
     </form>
 <?php
-    $bejegyzesek = sp("FAL", array(":userid" => $_SESSION["user"]));
-    foreach($bejegyzesek as $bejegyzes)
+    try
     {
-        echo("<div class=\"bejegyzes\">");
-        if ($bejegyzes["FELHASZNALO_ID"] == $_SESSION["user"]) {
-            echo("<div class=\"delete\"><a href=\"delete_post.php?p=".$bejegyzes["BEJEGYZES_ID"]."\">törlés</a></div>");
+        $conn = getConnection();
+
+        $bejegyzesek = callStoredProcedure($conn, "FAL", array(":userid" => $_SESSION["user"]));
+        foreach($bejegyzesek as $bejegyzes)
+        {
+            echo("<div class=\"bejegyzes\">");
+            if ($bejegyzes["FELHASZNALO_ID"] == $_SESSION["user"]) {
+                echo("<div class=\"delete\"><a href=\"delete_post.php?p=".$bejegyzes["BEJEGYZES_ID"]."\">törlés</a></div>");
+            }
+            echo("<div><img src=\"img.php?id=".$bejegyzes["KEP_ID"]."\" class=\"img\" /><div class=\"szerzo\">" . $bejegyzes["NEV"] . "</div></div>");
+            echo("<div class=\"szoveg\">".$bejegyzes["SZOVEG"]."</div>");
+
+            $kepek = query($conn, "SELECT KEP_ID FROM BEJEGYZES_KEP WHERE BEJEGYZES_ID = " . $bejegyzes["BEJEGYZES_ID"]);
+            foreach($kepek as $kep)
+            {
+                echo("<img src=\"img.php?id=".$kep["KEP_ID"]."\" class=\"wall_img\">");
+            }
+            echo("<div style=\"clear: both\"></div>");
+            echo("<div class=\"tagek\">".$bejegyzes["TAGEK"]."</div>");
+
+            echo("</div>");
         }
-        echo("<div><img src=\"img.php?id=".$bejegyzes["KEP_ID"]."\" class=\"img\" /><div class=\"szerzo\">" . $bejegyzes["NEV"] . "</div></div>");
-        echo("<div class=\"szoveg\">".$bejegyzes["SZOVEG"]."</div>");
-        echo("<div class=\"tagek\">".$bejegyzes["TAGEK"]."</div>");
-        echo("</div>");
+    }
+    catch(PDOException $e)
+    {
+        die($e->getMessage());
     }
 ?>
     </body>
