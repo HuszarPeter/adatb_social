4960552cf10c7b24ffbcf6d8cc57a638b27d3949
Peter Huszar <huszar.peter@sonrisa.hu>
2017-05-19 22:47:03 +0200

    start implementation of groups

---
+++ klub.php
@@ -0,0 +1,17 @@
+<?php
+require_once("lib/functions.php");
+?>
+
+<html>
+    <head>
+        <title>SOCIAL</title>
+        <link rel="stylesheet" type="text/css" href="css/social.css">
+    </head>
+    <body>
+    <?php include_once("fejlec.php");?>
+    <h1>Klubok</h1>
+<?php
+   
+?>
+    </body>
+</html>
