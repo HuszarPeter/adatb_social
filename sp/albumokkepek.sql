458f5b21a50ec362cc0ed53612b7d55b1db3bf0b
Peter Huszar <huszar.peter@sonrisa.hu>
2017-05-19 19:55:24 +0200

    resolve merge errors

--- sp/albumokkepek.sql
+++ sp/albumokkepek.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 select
   kep.kep_id,
   album.*
@@ -8,15 +7,4 @@ from
 where
   kep.tulajdonos_id = :userid
 order by
-=======
-select
-  kep.kep_id,
-  album.*
-from
-  kep
-  left join album on album.album_id = kep.album_id
-where
-  kep.tulajdonos_id = :userid
-order by
->>>>>>> origin/develop
   kep.album_id, kep.feltoltve
--- sp/nevnaposok.sql
+++ sp/nevnaposok.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 SELECT DISTINCT
     vfelhasznalo.*,
     CASE WHEN vfelhasznalo.NEVNAPHONAP = extract(month from sysdate) then 1 else 0 end as nevnap,
@@ -11,18 +10,4 @@ WHERE
     vismeros.felhasznalo_id = :userid and
     (vfelhasznalo.NEVNAPHONAP = extract(month FROM sysdate) or extract(month from vfelhasznalo.szuletett) = extract(month from sysdate))
  ORDER BY
-=======
-SELECT DISTINCT
-    vfelhasznalo.*,
-    CASE WHEN vfelhasznalo.NEVNAPHONAP = extract(month from sysdate) then 1 else 0 end as nevnap,
-    extract(month FROM vfelhasznalo.SZULETETT) as szulhonap,
-    extract(day from vfelhasznalo.SZULETETT) as szulnap
-FROM 
-    vismeros 
-    inner join vfelhasznalo on vfelhasznalo.felhasznalo_id = vismeros.ismeros_id
-WHERE 
-    vismeros.felhasznalo_id = :userid and
-    (vfelhasznalo.NEVNAPHONAP = extract(month FROM sysdate) or extract(month from vfelhasznalo.szuletett) = extract(month from sysdate))
- ORDER BY
->>>>>>> origin/develop
     nev
