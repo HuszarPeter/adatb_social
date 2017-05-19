b505142a1b93389c2d60ec1a375ae4f0319f0d0d
Peter Huszar <huszar.peter@sonrisa.hu>
2017-05-19 19:27:40 +0200

    albumok

--- fejlec.php
+++ fejlec.php
@@ -1,1 +1,1 @@
-<a href="profil.php">Profil</a> | <a href="wall.php">Fal</a> | <a href="baratok.php">Barátok</a> | <a href="messages.php">Üzenetek</a> | <a href="nevnap.php">Névnaposok, szülinaposok</a> | <a href="logout.php">Kijelentkezés</a>
+<a href="profil.php">Profil</a> | <a href="wall.php">Fal</a> | <a href="baratok.php">Barátok</a> | <a href="messages.php">Üzenetek</a> | <a href="nevnap.php">Névnaposok, szülinaposok</a> | <a href="albumok.php">Képek</a> | <a href="logout.php">Kijelentkezés</a>
