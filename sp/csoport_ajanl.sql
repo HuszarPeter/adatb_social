d636e19f8e5e6b5ed34901d1eb7afe03aa09c375
Peter Huszar <huszar.peter@sonrisa.hu>
2017-05-20 00:34:28 +0200

    csoport ajanlas

---
+++ sp/csoport_ajanl.sql
@@ -0,0 +1,19 @@
+select 
+csoport.csoport_id,
+csoport.NEV,
+csoport.LEIRAS,
+count(csoport_tag.CSOPORT_TAG_ID) as cnt
+from 
+    csoport 
+    inner join csoport_tag on csoport_tag.CSOPORT_ID = csoport.CSOPORT_ID
+where 
+    csoport.csoport_id in (
+        select distinct
+            csoport_tag.csoport_id
+        from
+            vismeros
+            inner join csoport_tag on csoport_tag.tag_felhasznalo_id = vismeros.ismeros_id
+        where 
+            felhasznalo_id = :userid and tipus = 1
+    )
+group by csoport.csoport_id, csoport.nev, csoport.LEIRAS
